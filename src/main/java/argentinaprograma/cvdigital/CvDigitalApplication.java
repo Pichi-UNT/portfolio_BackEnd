ebbd420ca800f18e0b6b942aa006e76ce7c93b8c
pichi <pichi3009@gmail.com>
2022-12-26 19:17:32 -0300

    first commit

---
+++ src/main/java/argentinaprograma/cvdigital/CvDigitalApplication.java
@@ -0,0 +1,13 @@
+package argentinaprograma.cvdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvDigitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/argentinaprograma/cvdigital/CvDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package argentinaprograma.cvdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
