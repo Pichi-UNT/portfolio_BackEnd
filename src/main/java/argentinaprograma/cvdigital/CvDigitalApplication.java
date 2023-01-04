44f1befde0c1f48dd86d7bbed28e2ecb94d37ba6
pichi <pichi3009@gmail.com>
2023-01-04 14:42:13 -0300

    Configuracion de autenticacion y autorizacion usando JWT con Spring Security

--- src/main/java/argentinaprograma/cvdigital/CvDigitalApplication.java
+++ src/main/java/argentinaprograma/cvdigital/CvDigitalApplication.java
@@ -2,10 +2,10 @@ package argentinaprograma.cvdigital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CvDigitalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CvDigitalApplication.class, args);
     }
